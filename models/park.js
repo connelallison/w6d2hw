4caedc673ff8a22e1b40af8f36a65570867ca780
Connel Allison <connelallison@hotmail.com>
2018-12-18 17:13:41 +0000

    initial commit

---
+++ models/park.js
@@ -0,0 +1,13 @@
+const Park = function (name, price, dinosaurs) {
+  this.name = name;
+  this.price = price;
+  this.dinosaurs = dinosaurs;
+}
+
+Park.prototype.addDinosaur = function(dinosaur) {
+  this.dinosaurs.push(dinosaur);
+};
+
+
+
+module.exports = Park;
---
+++ specs/park_spec.js
@@ -0,0 +1,58 @@
+const assert = require('assert');
+const Park = require('../models/park.js');
+const Dinosaur = require('../models/dinosaur.js');
+
+describe('Park', function() {
+
+  let park1;
+  let dinosaur1;
+  let dinosaur2;
+  let dinosaur3;
+  let dinosaur4;
+  let dinosaurs;
+
+  beforeEach(function () {
+    dinosaur1 = new Dinosaur("t-rex", 'carnivore', 150);
+    dinosaur2 = new Dinosaur("pterodactyl", 'carnivore', 50);
+    dinosaur3 = new Dinosaur("ankylosaurus", 'herbivore', 35);
+    dinosaur4 = new Dinosaur("oviraptorosaur", 'omnivore', 20);
+    dinosaurs = [dinosaur1, dinosaur2, dinosaur3, dinosaur4];
+    park1 = new Park("Jurassic Park", 500, dinosaurs);
+    dinosaur5 = new Dinosaur("triceratops", "herbivore", 50);
+  })
+
+  it('should have a name', function() {
+    assert.strictEqual(park1.name, "Jurassic Park");
+  });
+
+  it('should have a ticket price', function() {
+    assert.strictEqual(park1.price, 500);
+  });
+
+  it('should have a collection of dinosaurs', function() {
+    assert.strictEqual(park1.dinosaurs, dinosaurs);
+    assert.deepStrictEqual(park1.dinosaurs, [dinosaur1, dinosaur2, dinosaur3, dinosaur4]);
+  });
+
+  it('should be able to add a dinosaur to its collection', function() {
+    park1.addDinosaur(dinosaur5);
+    assert.strictEqual(park1.dinosaurs.length, 5);
+  });
+
+  xit('should be able to remove a dinosaur from its collection');
+
+  xit('should be able to find the dinosaur that attracts the most visitors');
+
+  xit('should be able to find all dinosaurs of a particular species');
+
+  xit("should be able to calculate the total number of visitors per day");
+
+  xit("should be able to calculate the total number of visitors per year");
+
+  xit("should be able to calculate the total revenue from ticket sales for one year");
+
+  xit('should be able to remove all dinosaurs of a particular species');
+
+  xit("should be able to provide an object containing the number of dinosaurs of each diet type");
+
+});
